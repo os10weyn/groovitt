9841db0c4a212c5952a764b7d37e5ca7f0fd747f
Austin Wayne De Guzman <111478174+os10weyn@users.noreply.github.com>
2022-12-13 14:24:38 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,11 +6,10 @@ import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import './css/loginform.css';
 import './css/signupform.css';
-<<<<<<< HEAD
-import './css/search.css'
-=======
 
->>>>>>> 3c3db15993f864fe16120d39473003bff3c06355
+import './css/search.css'
+
+
 import './css/homepage.css';
 // import './css/carousel.css';
 
@@ -21,8 +20,6 @@ import MPlayer from './js/playercomponents/index';
 <<<<<<< HEAD
 import Search from './js/search/search'
 
-=======
->>>>>>> 3c3db15993f864fe16120d39473003bff3c06355
 function App() {
   return (
     <Router>
@@ -40,12 +37,9 @@ function App() {
         <Route exact path='/player'>
           <MPlayer/>
         </Route>
-<<<<<<< HEAD
         <Route exact path='/search'>
           <Search />
         </Route>
-=======
->>>>>>> 3c3db15993f864fe16120d39473003bff3c06355
       </Switch>
     </div>
     </Router>
